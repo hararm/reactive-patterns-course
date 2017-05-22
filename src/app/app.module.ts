02237b0bc9f33e3b6d66de8592b9844e2a64b74e
hararm <avarmenhg@yahoo.com>
2017-05-22 15:21:01 -0400

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BrowserEventExperimentsComponent } from './browser-event-experiments/browser-event-experiments.component';
+import { EventBusExprimentsComponent } from './event-bus-expriments/event-bus-expriments.component';
+import { LessonListComponent } from './lesson-list/lesson-list.component';
+import { LessonsCounterComponent } from './lessons-counter/lessons-counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BrowserEventExperimentsComponent,
     BrowserEventExperimentsComponent,
-    BrowserEventExperimentsComponent
+    BrowserEventExperimentsComponent,
+    EventBusExprimentsComponent,
+    LessonListComponent,
+    LessonsCounterComponent
   ],
   imports: [
     BrowserModule,
